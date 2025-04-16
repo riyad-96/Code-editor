16c168e1df0b3215cffc6230a7e71571743b945a
Riyad <onlygobs0@gmail.com>
2025-04-17 05:52:27 +0600

    saved code loaded feature complete

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const htmlInput = document.getElementById('html')
 const cssInput = document.getElementById('css')
 const jsInput = document.getElementById('js')
-
 const runBtn = document.querySelector('.run-btn')
 
+//! Run codes on clicking run button
 function runCode() {
   const html = htmlInput.value;
   const css = `<style>${cssInput.value}</style>`;
@@ -17,14 +17,38 @@ runBtn.addEventListener('click', runCode)
 
 
 //! auto run the runCode function 
-let timeout
+let timeout;
 
 function autoRunCode() {
   clearTimeout(timeout)
 
   timeout = setTimeout(() => {
     runCode()
-  }, 1600)
+  }, 1400)
 }
 
-[htmlInput, cssInput, jsInput].forEach(input => input.addEventListener('input', autoRunCode))
+
+//! Save code to local storage
+function saveToLocalStorage() {
+  localStorage.setItem('htmlCode', htmlInput.value)
+  localStorage.setItem('cssCode', cssInput.value)
+  localStorage.setItem('jsCode', jsInput.value)
+  
+  autoRunCode()
+}
+
+
+[htmlInput, cssInput, jsInput].forEach(input => input.addEventListener('input', saveToLocalStorage))
+
+//! Load everything on screen refresh
+window.addEventListener('DOMContentLoaded', () => {
+  const savedHTML = localStorage.getItem('htmlCode')
+  const savedCSS = localStorage.getItem('cssCode')
+  const savedJS = localStorage.getItem('jsCode')
+
+  if(savedHTML) htmlInput.value = savedHTML
+  if(savedCSS) cssInput.value = savedCSS
+  if(savedJS) jsInput.value = savedJS
+
+  runCode()
+})
